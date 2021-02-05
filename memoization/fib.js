f2138a9b3768988e9244127e2562b2871fb37a55
xtropia <ufocalypse@gmail.com>
2021-02-05 17:14:38 +0545

    Use memoization to reduce the time complexity of calcFib function from exponential to linear

--- memoization/fib.js
+++ memoization/fib.js
@@ -1,11 +1,11 @@
-const calcFib = (n) => {
+const calcFib = (n) => { // this has O(2^n) time complexity
     if (n === 0 || n === 1) {
         return 1
     }
     return calcFib(n - 1) + calcFib(n - 2)
 }
 
-const n = Number.parseInt(process.argv[2])
+const n = Number.parseInt(process.argv[2]) || 10
 
 const result = { input: n, result: calcFib(n)}
 console.log(result)
---
+++ memoization/fibMemo.js
@@ -0,0 +1,17 @@
+const calcFib = (function() {
+    const memo = {}
+    return (n) => {
+        if (n === 0 || n === 1) {
+            return 1
+        }
+        if (!memo[n]) {
+            memo[n] = calcFib(n - 1) + calcFib(n - 2)
+        }
+        return memo[n]
+    }
+}())
+
+const n = Number.parseInt(process.argv[2]) || 10
+
+const result = { input: n, result: calcFib(n)}
+console.log(result)
